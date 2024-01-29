285cada9660c71956f4499403ee0ca47835cfc3d
kowalsky <kowalsky2608@wp.pl>
2024-01-29 20:19:00 +0100

    add styles

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const port = 3000
 const app = express()
 app.set('view engine','hbs')
 
+app.use(express.static('public'))
+
 app.get('/', (req,res) => {
     res.render('index')
 })
 app.get('/kontakt', (req,res) => {
     res.send('dane node')
 })
+
+
 
 app.listen(port)
